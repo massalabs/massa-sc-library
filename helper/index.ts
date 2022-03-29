874ae4fb87d3abc57c6ee800a8a72c4fe1facd17
Grégory Libert <Grgory Libert>
2022-03-29 20:57:36 +0200

    correcting relative path result import

--- helper/index.ts
+++ helper/index.ts
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export * from './result/result';
 =======
 export * from '.result/result';
 >>>>>>> ee5b47d (initial commit)
+=======
+export * from "./result/result";
+>>>>>>> d4ee11c (correcting relative path result import)

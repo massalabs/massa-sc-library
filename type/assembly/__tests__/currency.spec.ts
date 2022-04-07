17605e470c93b0299c9601066d90b70fbd0d1c26
Grégory Libert <Grgory Libert>
2022-04-07 10:01:31 +0200

    linter

--- type/assembly/__tests__/currency.spec.ts
+++ type/assembly/__tests__/currency.spec.ts
@@ -12,17 +12,17 @@ describe('Doc tests', () => {
 });
 
 describe('Black box tests', () => {
-    test("empty constructor", () => {
-        const c = new Currency();
-        expect<string>(c.name()).toBe('');
-        expect<u8>(c.minorUnit()).toBe(0);
-    })
+  test('empty constructor', () => {
+    const c = new Currency();
+    expect<string>(c.name()).toBe('');
+    expect<u8>(c.minorUnit()).toBe(0);
+  });
 
-    test("same currency", () => {
-        const c1 = new Currency("aaaa", 6);
-        const c2 = new Currency("aaaa", 6);
-        expect<bool>(c1.sameAs(c2)).toBeTruthy();
-        expect<bool>(c2.sameAs(c1)).toBeTruthy();
-        expect<bool>(c1.sameAs(c1)).toBeTruthy();
-    })
-})
+  test('same currency', () => {
+    const c1 = new Currency('aaaa', 6);
+    const c2 = new Currency('aaaa', 6);
+    expect<bool>(c1.sameAs(c2)).toBeTruthy();
+    expect<bool>(c2.sameAs(c1)).toBeTruthy();
+    expect<bool>(c1.sameAs(c1)).toBeTruthy();
+  });
+});
